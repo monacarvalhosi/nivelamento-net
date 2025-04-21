766df953609227841c3641fa5763284082c1e435
monaliza <mona.carvalho.si@gmail.com>
2025-04-21 19:43:29 -0300

    feat (questao1): information banking account

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -2,8 +2,28 @@
 
 namespace Questao1
 {
-    class ContaBancaria {
+    public class ContaBancaria {
 
-       
+        private double _saldoConta;
+        public double SaldoConta => _saldoConta;
+
+        public long NumeroConta {  get; init; }
+        public string TitularConta { get; set; }
+
+        //numero, titular, depositoInicial)
+        public ContaBancaria(long numero,string titular, double depositoInicial = 0)
+        {
+            NumeroConta = numero;
+            TitularConta = titular;
+            _saldoConta += depositoInicial;
+        }
+
+        public void Deposito(double valorDeposito) => _saldoConta += valorDeposito;
+        public void Saque(double valorSaque) => _saldoConta -= valorSaque + 3.50;
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {TitularConta}, Saldo $ {SaldoConta:F2}";
+        }
     }
 }
